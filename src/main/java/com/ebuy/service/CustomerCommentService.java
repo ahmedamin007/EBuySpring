644651ddc4645a1dc3f9bbab11133e19b359157a
ahmedamin007 <ahmed_amin007@hotmail.com>
2016-06-14 21:30:52 -0500

    ahmed user commit

--- src/main/java/com/ebuy/service/CustomerCommentService.java
+++ src/main/java/com/ebuy/service/CustomerCommentService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ebuy.model.CustomerComment;
+import com.ebuy.model.Product;
 import com.ebuy.repository.CustomerCommentRepository;
 
 @Service
@@ -20,17 +21,17 @@ public class CustomerCommentService {
 	public CustomerComment save(CustomerComment CustomerComment){
 		return commentRepository.save(CustomerComment);
 	}
-	
-	
+
 	public List<CustomerComment> findAll(){
 		return commentRepository.findAll();
-		
 	}
-	
 
 	public void delete(CustomerComment CustomerComment){
 		commentRepository.delete(CustomerComment);
-		
+	}
+	
+	public List<CustomerComment> findByProduct(Product product){
+		return commentRepository.findByProduct(product);
 	}
 
 }
